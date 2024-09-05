5ef52f0688ada138c403f31774d2dcfecb677a36
unknown <pvengalarevanth22@gmail.com>
2024-09-05 17:32:13 -0400

    Add comments

--- Singleton/DbConn.java
+++ Singleton/DbConn.java
@@ -4,15 +4,21 @@ public class DbConn {
     private String Url;
     private String userName;
     private String password;
+
+    //Create static variable so that it will be at static level
     private static DbConn inst;
 
+    //locks for multi threading application
     private static ReentrantLock lock=new ReentrantLock();
+
+    //Making constructor private so that client can't access it
     private DbConn(String Url,String userName,String password){
         this.Url=Url;
         this.userName=userName;
         this.password=password;
     }
 
+    //Dual null check locking
     public static DbConn getInstance(String Url,String userName,String password){
         if(inst==null){
             lock.lock();
